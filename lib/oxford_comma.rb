6b9c6081f7e32cbdd5650ade2f9942858cb5f320
Philip Warren <philwarren1@gmail.com>
2020-07-24 15:41:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
- array = ["kiwi"]
- array << (" and durian")
- array << (", and starfruit")
- array.join
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
-["a", "b", "c"].join
